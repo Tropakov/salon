a26d2321c06e0271eb56f53977e5cf2ed18afa8d
Tropakov <115077626+Tropakov@users.noreply.github.com>
2022-11-25 14:25:22 +0700

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salon
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            postavshik Win = new postavshik();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Owner.Show();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Menedger Win = new Menedger();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Prodazhi Win = new Prodazhi();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            klienti Win = new klienti();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            spravki Win = new spravki();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+    }
+}
---
+++ Menedger.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace salon
+{
+    public partial class Menedger : Form
+    {
+        public Menedger()
+        {
+            InitializeComponent();
+            getinfo();
+        }
+
+        private void getinfo()
+        {
+            string query = "SELECT id_men, fio, telefon, adres FROM menedger";
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            MySqlDataAdapter sda = new MySqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            try
+            {
+                conn.Open();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                dataGridView1.ClearSelection();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла непредвиденная ошибка!" + Environment.NewLine + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Owner.Show();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("Server=localhost;Database=salon;User=root;Password=2633;");
+                MySqlCommand cmd = new MySqlCommand("insert into menedger(id_men, fio, telefon, adres) value('" + textBox1.Text + "','" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "');", conn);
+                conn.Open();
+                cmd.ExecuteReader();
+                conn.Close();
+                getinfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка внесения данных!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы уверены, что хотите изменить данные?", "Подтвердите свои действия", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("Server=localhost;Database=salon;User=root;Password=2633;");
+                MySqlCommand cmd = new MySqlCommand("update menedger set id_men ='" + textBox1.Text + "', fio = '" + textBox2.Text + "', telefon = '" + textBox3.Text + "', adres = '" + textBox4.Text + "' where id_men =" + dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString() + ";", conn);
+                Console.WriteLine("update menedger set id_men ='" + textBox1.Text + "', fio = '" + textBox2.Text + "', telefon = '" + textBox3.Text + "', adres = '" + textBox4.Text + "' where id_men =" + dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString() + ";");
+                conn.Open();
+                cmd.ExecuteReader();
+                conn.Close();
+                getinfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DialogResult res = MessageBox.Show("Вы уверены что хотите удалить информацию?", "Подтвердите действие", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (res == DialogResult.Yes)
+                {
+                    string del = "delete from menedger where id_men = " + dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString() + ";";
+                    do_Action(del);
+                    getinfo();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не выбрано ни одной записи! Удаление невозможно.");
+            }
+        }
+        private void do_Action(string query)
+        {
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            MySqlCommand cmDB = new MySqlCommand(query, conn);
+            try
+            {
+                conn.Open();
+                MySqlDataReader rd = cmDB.ExecuteReader();
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Произшла непредвиденная ошибка!");
+            }
+        }
+    }
+}
